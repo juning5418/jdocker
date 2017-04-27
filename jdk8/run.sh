52a905468eeefb3c6ee095fe88d4cac1602eb015
juning <691092674@qq.com>
2017-04-27 11:00:02 +0800

    add -d

--- jdk8/run.sh
+++ jdk8/run.sh
@@ -1,1 +1,1 @@
-docker run -p 9999:22 -t -i "jdocker/jdk8:v1"
+docker run -p 9999:22 -d -t -i "jdocker/jdk8:v1"
--- tomcat/run.sh
+++ tomcat/run.sh
@@ -1,1 +1,1 @@
-docker run -p 9999:22 -p 8081:8080 -t -i jdocker/tomcat8:v1
+docker run -p 9999:22 -p 8081:8080 -d -t -i  jdocker/tomcat8:v1
