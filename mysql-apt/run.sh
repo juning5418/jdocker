f44dec1567835855708d64ca21c875a5d7a6fffb
juning <691092674@qq.com>
2017-04-28 02:43:21 +0800

    mysql run.sh

--- mysql-apt/run.sh
+++ mysql-apt/run.sh
@@ -1,2 +1,1 @@
-docker run -p 9992:22 -p 3307:3306 -d -t -i -v /docker/mysql/data:/var/lib/mysql jdocker/mysql-apt:v1
-
+docker run -p 9992:22 -p 3307:3306 -d -t -i -v /root/docker/mysql/data:/var/lib/mysql jdocker/mysql-apt:v1
