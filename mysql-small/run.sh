244dbf7b772ef218dcfeaa2a768d425f100b5d6e
juning <691092674@qq.com>
2017-04-28 11:38:51 +0800

    mysql-small

--- mysql-small/run.sh
+++ mysql-small/run.sh
@@ -1,2 +1,3 @@
 #docker run -p 9992:22 -p 3307:3306 -d -t -i -v /root/docker/mysql/data:/var/lib/mysql jdocker/mysql-apt:v1
-docker run -p 9992:22 -p 3307:3306 -d  -t -i -v /root/docker/mysql/data:/var/lib/mysql jdocker/mysql-small:v1
+docker run -p 9992:22 -p 3307:3306  -e MYSQL_ROOT_PASSWORD=rootroot -d  -t -i -v /root/docker/mysql/data:/var/lib/mysql jdocker/mysql-small:v1
+
