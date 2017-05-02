12784a5366f37f74ca98929aed4930b3e8dfa2d8
juning <691092674@qq.com>
2017-05-02 09:36:54 +0800

    mysql big

--- mysql-big/run.sh
+++ mysql-big/run.sh
@@ -1,2 +1,2 @@
-docker run -p 9992:22 -p 3307:3306 -t -i -v /docker/mysql/data:/software/mysql-5.6.28/data jdocker/mysql:v1
+docker run -p 9992:22 -p 3307:3306 -t -i -v /root/docker/mysql/data:/software/mysql-5.6.28/data jdocker/mysql:v1
 
