4032771deeaaf02fe324bafc1de696da221cf908
juning <691092674@qq.com>
2017-05-08 16:16:44 +0800

    es

---
+++ elasticsearch-latest/build.sh
@@ -0,0 +1,4 @@
+
+
+#sudo docker run -d -v /home/cadmin/config/elasticsearch/config:/usr/share/elasticsearch/config -p 9200:9200 -p 9300:9300  --name=elasticsearch-test elasticsearch:latest
+#sudo docker run -d -p 9200:9200 -p 9300:9300  --name=elasticsearch-test elasticsearch:latest
