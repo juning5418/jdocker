dfcb1a96bc0f50edbf804b147970337a4656aabb
juning <691092674@qq.com>
2017-05-08 15:05:38 +0800

    es  Please enter the commit message for your changes. Lines starting

--- elasticsearch-5.3.0/run.sh
+++ elasticsearch-5.3.0/run.sh
@@ -1,8 +1,10 @@
- sudo docker run -d -p 9200:9200 -p 9300:9300    --name=elasticsearch-test elasticsearch:latest
+ #sudo docker run -d -p 9200:9200 -p 9300:9300    --name=elasticsearch-test elasticsearch:latest
 
-  /usr/share/elasticsearch/config/elasticsearch.yml
+#/usr/share/elasticsearch/config/elasticsearch.yml
+
+#sudo docker run -d -p 9200:9200  -v /home/cadmin/data/elasticsearch/config:/usr/share/elasticsearch/config -v /home/cadmin/data/elasticsearch/data:/usr/share/elasticsearch/data elasticsearch:latest
+
+#sudo docker run -d -p 9201:9200  --name=elasticsearch-test9201 -v "/home/cadmin/data/elasticsearch/config":/usr/share/elasticsearch/config -v "/home/cadmin/data/elasticsearch/data":/usr/share/elasticsearch/data elasticsearch:latest
 
 
-sudo docker run -d -p 9200:9200  -v /home/cadmin/data/elasticsearch/config:/usr/share/elasticsearch/config -v /home/cadmin/data/elasticsearch/data:/usr/share/elasticsearch/data elasticsearch:latest
-
-sudo docker run -d -p 9201:9200  --name=elasticsearch-test9201 -v "/home/cadmin/data/elasticsearch/config":/usr/share/elasticsearch/config -v "/home/cadmin/data/elasticsearch/data":/usr/share/elasticsearch/data elasticsearch:latest
+docker run -p 9999:22 -p 9200:9200 -p 9300:9300 -t -i 'jdocker/elasticsearch:v1'
