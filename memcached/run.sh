f8ce99d6c268a2b1a7260429b1b039879307239e
juning <691092674@qq.com>
2017-05-16 14:06:10 +0800

    add

--- memcached/run.sh
+++ memcached/run.sh
@@ -1,2 +1,2 @@
 #docker run --name memcached-wbkt  -p 40232:11211 -d -t -i "jdocker/memcached:v1"
-docker run --name memcached-demo -p 9994:22 -p 11212:11211 -d -t -i "jdocker/memcached:v1"
+docker run -v /etc/localtime:/etc/localtime:ro --name memcached-demo -p 9994:22 -p 11212:11211 -d -t -i "jdocker/memcached:v1"
