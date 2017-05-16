e45dbf8112507e3ef3a4a8ec7583c112a54ec35c
juning <691092674@qq.com>
2017-05-16 14:38:17 +0800

    add

--- mysql-small/run.sh
+++ mysql-small/run.sh
@@ -6,3 +6,4 @@
 #docker run --name mysql -v /home/cadmin/data/mysql/:/var/lib/mysql  -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql:latest
 
 docker run --name mysql   -p 3307:3306 -d  -v /etc/localtime:/etc/localtime:ro   -v /root/data/mysql/:/var/lib/mysql jdocker/mysql-small:v1
+#docker run --name mysql   -p 3307:3306 -d  -v /etc/localtime:/etc/localtime:ro   jdocker/mysql-small:v1
