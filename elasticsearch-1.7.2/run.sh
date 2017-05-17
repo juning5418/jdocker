38f013edb69ec64fb83c9d3d87bb18b673edb681
juning <691092674@qq.com>
2017-05-17 20:56:20 +0800

    add

--- elasticsearch-1.7.2/run.sh
+++ elasticsearch-1.7.2/run.sh
@@ -1,1 +1,2 @@
-docker run --name elasticsearch1.7.2  -v /home/cadmin/data/elasticsearch/data:/software/elasticsearch-1.7.2/data  -p 9200:9200 -p 9300:9300 -t -i -d 'jdocker/elasticsearch:v1.7.2'
+# docker run --name elasticsearch1.7.2  -v /home/cadmin/data/elasticsearch/data:/software/elasticsearch-1.7.2/data  -p 9200:9200 -p 9300:9300 -t -i -d 'jdocker/elasticsearch:v1.7.2'
+docker run --name elasticsearch1.7.2  -v /root/data/elasticsearch/data:/software/elasticsearch-1.7.2/data  -p 9200:9200 -p 9300:9300 -t -i -d 'jdocker/elasticsearch:v1.7.2'
