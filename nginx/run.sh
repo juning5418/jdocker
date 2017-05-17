1083b0fb418add05ced9c29e76a785bc8293e0a9
juning <691092674@qq.com>
2017-05-17 17:14:09 +0800

    add

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,1 +1,1 @@
-docker run -p 9991:22 -p 80:80 -v /root/docker/nginx:/ -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
+docker run -p 80:80 -v /root/docker/nginx:/software/nginx-1.8.0 -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
