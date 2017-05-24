87ab4e8b821e10fac264d8440bc1930c377ac61d
juning <691092674@qq.com>
2017-05-24 15:39:44 +0800

    add

--- tomcat/run.sh
+++ tomcat/run.sh
@@ -1,2 +1,3 @@
 #docker run --name tomcat-kanfang-8081  -v  /home/cadmin/file:/file -v /etc/localtime:/etc/localtime:ro -v /home/cadmin/war/kanfang:/usr/local/apache-tomcat-8.5.14/webapps/  -p 8081:8080 -d -t -i  jdocker/tomcat8:v1
 docker run --name tomcat-web-8081  -v  /root/file:/file -v /etc/localtime:/etc/localtime:ro -v /root/war/web:/usr/local/apache-tomcat-8.5.14/webapps/  -p 8081:8080 -d -t -i  jdocker/tomcat8:v1
+docker run --name tomcat-hybs-8082  -v  /root/file:/file -v /etc/localtime:/etc/localtime:ro -v /root/war/hybs:/usr/local/apache-tomcat-8.5.14/webapps/  -p 8082:8080 -p 11181:11181 -p 11187:11187 -d -t -i  jdocker/tomcat8:v1
