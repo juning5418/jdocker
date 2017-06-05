976e963379ec16ed71241503519a6b74d182ed63
juning <691092674@qq.com>
2017-06-05 16:48:06 +0800

    add

---
+++ runindex.sh
@@ -0,0 +1,2 @@
+curl -XPUT --user elastic:adminvalue http://121.41.12.245:9200/index;
+
--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,4 @@
 
-curl -XPUT --user elastic:adminvalue http://121.41.12.245:9200/index;
-
 
 curl -XPOST --user elastic:adminvalue http://121.41.12.245:9200/index/fulltext/_mapping -d'
 {
