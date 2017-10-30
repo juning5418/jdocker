e315ac1c066f2251a2de1cb2ee5b34ca3191853c
juning <691092674@qq.com>
2017-10-30 10:52:15 +0800

    add

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,3 +1,3 @@
 docker run -p 80:80 -v /root/file:/software/file -v /root/war:/software/war -v /root/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
 docker run --name nginx --restart=always -p 80:80 -v /file/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
-docker run --name nginx --restart=always -p 80:80  -v /home/cadmin/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro   -d -t -i jdocker/nginx:v1
+docker run --name nginx --restart=always -p 80:80  -v /home/cadmin/logs:/software/logs -v /home/cadmin/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro   -d -t -i jdocker/nginx:v1
