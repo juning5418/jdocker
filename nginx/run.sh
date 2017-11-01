2c1eabe22c51b875c5db36e76a72ad464ad4e308
juning <691092674@qq.com>
2017-11-01 18:18:04 +0800

    add

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,3 +1,7 @@
 #docker run -p 80:80 -v /root/file:/software/file -v /root/war:/software/war -v /root/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
 #docker run --name nginx --restart=always -p 80:80 -v /file/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro  -d -t -i jdocker/nginx:v1
-docker run --name nginx --restart=always -p 80:80   -v /home/cadmin/logs:/software/logs -v /home/cadmin/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro   -d -t -i jdocker/nginx:v1
+# docker run --name nginx --restart=always -p 80:80   -v /home/cadmin/logs:/software/logs -v /home/cadmin/file:/software/file -v /home/cadmin/war:/software/war -v /home/cadmin/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro   -d -t -i jdocker/nginx:v1
+
+
+
+docker run --name nginx --restart=always -p 8891:80   -v /Users/juning/nginx/logs:/usr/local/nginx/logs -v /Users/juning/nginx/file:/software/file -v /Users/juning/nginx/war:/software/war -v /Users/juning/nginx/servers:/software/nginx-1.8.0/servers -v /etc/localtime:/etc/localtime:ro   -d -t -i jdocker/nginx:v1
